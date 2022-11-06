9cf3d383e9b3aaeef33fffe32f8edd936032e3c3
Adam Del Sol <adamdelsolrun@gmail.com>
2022-11-06 10:10:29 -0500

    removed factory pattern

--- test/RadioStarFactory.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-import "forge-std/Test.sol";
-import "../src/RadioStarFactory.sol";
-
-contract RadioStarFactoryTest is Test {
-    function setUp() public {}
-}
