fa537d3af979cf127ac86af4ddafcaa02422656a
jarryingnut <rabhishekranjan@gmail.com>
2022-11-07 23:03:57 +0530

    impliment mvp v1

--- src/RadioStar.sol
+++ src/RadioStar.sol
@@ -10,7 +10,12 @@ contract RadioStar is ERC1155 {
     mapping(uint256 => address) public tokensToArtist;
     mapping(uint256 => uint256) private tokensToPrice;
 
-    event RadioStarCreated(address artistAccount, uint256 tokenId, uint256 supply, uint256 priceInHundredthsOfEther);
+    event RadioStarCreated(
+        address artistAccount,
+        uint256 tokenId,
+        uint256 supply,
+        uint256 priceInHundredthsOfEther
+    );
     event RadioStarPurchased(address fanAccount, uint256 tokenId);
 
     constructor(string memory uri) ERC1155(uri) {
@@ -19,22 +24,35 @@ contract RadioStar is ERC1155 {
     }
 
     // Function for an artist to create a RadioStar Song NFT for purchase
-    function createRadioStar(uint256 supply, uint256 priceInHundredthsOfEther) external {
+    function createRadioStar(uint256 supply, uint256 priceInGwei) external {
+        require(
+            priceInWei >= 10000000,
+            "listing price should be greater than 0.01 eth"
+        );
         tokenId++;
         tokensToArtist[tokenId] = msg.sender;
         tokensToPrice[tokenId] = priceInHundredthsOfEther;
         _mint(owner, tokenId, supply, "");
-        emit RadioStarCreated(msg.sender, tokenId, supply, priceInHundredthsOfEther);
+        emit RadioStarCreated(
+            msg.sender,
+            tokenId,
+            supply,
+            priceInHundredthsOfEther
+        );
     }
 
     // Function for a fan to purchase a RadioStar Song NFT
     function mintRadioStar(uint256 _tokenId) external payable {
-        uint256 songPrice = tokensToPrice[_tokenId];
-        // TODO: require payment to >= songPrice
+        require(
+            tokensToPrice[_tokenId] <= msg.value,
+            "the price should be greater or equal to the listing price"
+        );
         address artistAccount = tokensToArtist[_tokenId];
         // TODO: send 2% of funds to owner
         // TODO: send remainer of funds to artistAccount
         safeTransferFrom(owner, msg.sender, _tokenId, 1, "");
         emit RadioStarPurchased(msg.sender, _tokenId);
     }
+
+    receive() external payable {}
 }
