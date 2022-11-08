88e892cb8bcfba0f0af930253eff7c279636f253
Adam Del Sol <adamdelsolrun@gmail.com>
2022-11-07 21:50:06 -0500

    began success testing

--- src/RadioStar.sol
+++ src/RadioStar.sol
@@ -4,40 +4,41 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/token/ERC1155/erc1155.sol";
 
 contract RadioStar is ERC1155 {
-    address public owner;
+    address private _owner;
+    // TokenId 0 will not be associated with a token
     uint256 public tokenId = 0;
 
     mapping(uint256 => address) public tokensToArtist;
-    mapping(uint256 => uint256) private tokensToPrice;
+    mapping(uint256 => uint256) public tokensToPrice;
 
     event RadioStarCreated(
-        address artistAccount,
-        uint256 tokenId,
+        address indexed artistAccount,
+        uint256 indexed tokenId,
         uint256 supply,
-        uint256 priceInHundredthsOfEther
+        uint256 priceInGwei
     );
-    event RadioStarPurchased(address fanAccount, uint256 tokenId);
+    event RadioStarPurchased(address indexed fanAccount, uint256 indexed tokenId);
 
     constructor(string memory uri) ERC1155(uri) {
         _setURI(uri);
-        owner = msg.sender;
+        _owner = msg.sender;
     }
 
     // Function for an artist to create a RadioStar Song NFT for purchase
     function createRadioStar(uint256 supply, uint256 priceInGwei) external {
         require(
-            priceInWei >= 10000000,
+            priceInGwei >= 10000000,
             "listing price should be greater than 0.01 eth"
         );
         tokenId++;
         tokensToArtist[tokenId] = msg.sender;
-        tokensToPrice[tokenId] = priceInHundredthsOfEther;
-        _mint(owner, tokenId, supply, "");
+        tokensToPrice[tokenId] = priceInGwei;
+        _mint(_owner, tokenId, supply, "");
         emit RadioStarCreated(
             msg.sender,
             tokenId,
             supply,
-            priceInHundredthsOfEther
+            priceInGwei
         );
     }
 
@@ -47,10 +48,7 @@ contract RadioStar is ERC1155 {
             tokensToPrice[_tokenId] <= msg.value,
             "the price should be greater or equal to the listing price"
         );
-        address artistAccount = tokensToArtist[_tokenId];
-        // TODO: send 2% of funds to owner
-        // TODO: send remainer of funds to artistAccount
-        safeTransferFrom(owner, msg.sender, _tokenId, 1, "");
+        safeTransferFrom(_owner, msg.sender, _tokenId, 1, "");
         emit RadioStarPurchased(msg.sender, _tokenId);
     }
 
--- test/RadioStar.t.sol
+++ test/RadioStar.t.sol
@@ -2,8 +2,60 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../src/RadioStar.sol";
 
 contract RadioStarTest is Test {
-    function setUp() public {}
+    RadioStar radioStar;
+    address radioStarOwner = address(0x1);
+    address radioStarArtist = address(0x2);
+    address radioStarFan = address(0x3);
+
+    event RadioStarCreated(
+        address indexed artistAccount,
+        uint256 indexed tokenId,
+        uint256 supply,
+        uint256 priceInGwei
+    );
+    event RadioStarPurchased(address indexed fanAccount, uint256 indexed tokenId);
+    
+    function setUp() public {
+        vm.prank(radioStarOwner);
+        radioStar = new RadioStar("https://whatever/");
+    }
+
+    function testCreateRadioStar() public {
+        vm.startPrank(radioStarArtist);
+        uint256 _supply = 100;
+        uint256 _priceInGwei = 20000000;
+
+        assertEq(0, uint(radioStar.tokenId()), "Token Id not initialized to 0");
+        
+        vm.expectEmit(true, true, true, true);
+        emit RadioStarCreated(radioStarArtist, 1, _supply, _priceInGwei);
+
+        radioStar.createRadioStar(_supply, _priceInGwei);
+
+        assertEq(radioStar.tokensToArtist(1), radioStarArtist, "TokenId not mapped to Artist's Account");
+        assertEq(radioStar.tokensToPrice(1), _priceInGwei, "tokenId not mapped to price");
+        assertEq(1, uint(radioStar.tokenId()), "Token ID not incremented");
+        vm.stopPrank();
+    }
+
+    function testMintRadioStar() public {
+        vm.startPrank(radioStarArtist);
+        uint256 _supply = 100;
+        uint256 _priceInGwei = 20000000;
+        radioStar.createRadioStar(_supply, _priceInGwei);
+        vm.stopPrank();
+
+        vm.deal(radioStarFan, 10000e18);
+        vm.startPrank(radioStarFan);
+
+        vm.expectEmit(true, true, true, true); 
+        emit RadioStarPurchased(radioStarFan, 1);
+
+        radioStar.mintRadioStar{value: _priceInGwei}(1);
+        vm.stopPrank();
+    }
 }
